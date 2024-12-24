31e3e945c60621fc732bdc916afb478dd49f6f7b
Gabriel <gurufiredark@gmail.com>
2024-12-24 03:19:49 -0300

    next config att

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
-module.exports = {
+const nextConfig = {
+  output: 'export',  // Necessário para GitHub Pages
   images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'openweathermap.org',
-        },
-      ],
-    }
+    unoptimized: true,  // Necessário quando usar export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   }
+}
+
+module.exports = nextConfig
